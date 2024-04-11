d3b42b4e421d886fef89556631189272246f34fb
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-10 22:15:19 -0300

    ALLOWED_HOSTS

--- imageclassify/imageclassify/settings.py
+++ imageclassify/imageclassify/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("SECRET_KEY", "")
 # SECURITY WARNING: don't run withenv("DEBUG") debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
