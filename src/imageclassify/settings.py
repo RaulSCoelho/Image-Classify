b0373c6cfd100d7d5ca2691e22ee8842547ea723
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-11 00:51:44 -0300

    read_env() override and static files ignored

--- src/imageclassify/settings.py
+++ src/imageclassify/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env()  # read .env file, if it exists
+env.read_env(override=True)  # read .env file, if it exists
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
