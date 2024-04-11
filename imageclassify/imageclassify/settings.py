92954ebbfcba22b10aaf0d9b1d2b819bbb27f9e6
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-11 15:44:09 -0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS in settings.py

--- imageclassify/imageclassify/settings.py
+++ imageclassify/imageclassify/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run withenv("DEBUG") debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(';')
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 
 # Application definition
