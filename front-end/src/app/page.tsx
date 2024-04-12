fd73e2f521245bf6b11dbeb01b8bf49ac6243110
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-12 00:36:48 -0300

    front-end

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useForm } from 'react-hook-form'
 import { useApiArrayState } from '@/hooks/use-api-state'
 import { PredictInput, PredictOutput, Prediction, predictSchema } from '@/types/prediction'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Input } from '@nextui-org/react'
+import { Button, Input, ScrollShadow } from '@nextui-org/react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -39,27 +39,32 @@ export default function Home() {
 
   return (
     <div className="flex max-h-[100dvh] flex-col gap-3 p-5 sm:p-20">
-      <form onSubmit={handleSubmit(onSubmit as any)}>
-        {image?.name || 'No file selected'}
+      <form
+        className="space-y-2 rounded-medium bg-white/60 p-5 pb-2 text-center"
+        onSubmit={handleSubmit(onSubmit as any)}
+      >
+        <p className="text-lg font-bold">{image?.name || 'No file selected'}</p>
         <Input type="file" onChange={onChangeImage} errorMessage={errors.image?.message} isInvalid={!!errors.image} />
         <Button type="submit" color="secondary" isLoading={isSubmitting}>
-          Predict
+          Guess
         </Button>
       </form>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-2 overflow-auto">
-        {predictions?.map((p: Prediction) => (
-          <div className="overflow-hidden rounded-md bg-white/60" key={p.id}>
-            <div className="relative h-40">
-              <Image src={p.image} alt={p.label} objectFit="cover" fill />
+      <ScrollShadow hideScrollBar>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-2">
+          {predictions?.map((p: Prediction) => (
+            <div className="overflow-hidden rounded-medium bg-white/60" key={p.id}>
+              <div className="relative h-40">
+                <Image src={p.image} alt={p.label} objectFit="cover" fill />
+              </div>
+              <div className="p-3">
+                <p>{p.label}</p>
+                <p>{p.prediction}</p>
+                <p>{p.updated_at}</p>
+              </div>
             </div>
-            <div className="p-3">
-              <p>{p.label}</p>
-              <p>{p.prediction}</p>
-              <p>{p.updated_at}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </ScrollShadow>
     </div>
   )
 }
