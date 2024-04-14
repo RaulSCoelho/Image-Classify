02da0f0247d283e59c3459d6c13048924b037042
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-14 14:58:52 -0300

    AIModels under construction

--- front-end/src/app/models/index.tsx
+++ front-end/src/app/models/index.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { Maintenance } from '@/components/maintenance'
+
 export function AIModels() {
   return (
-    <div>
-      <h1>Models</h1>
+    <div className="p-5">
+      <Maintenance
+        title="Under Construction"
+        description="We're currently working on this page to bring you something amazing. Stay tuned!"
+      />
     </div>
   )
 }
---
+++ front-end/src/components/maintenance.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaGear } from 'react-icons/fa6'
+
+interface MaintenanceProps {
+  title: string
+  description: string
+}
+
+export function Maintenance({ title, description }: MaintenanceProps) {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="text-center">
+        <FaGear size={60} className="mx-auto mb-4 animate-[spin_4s_linear_infinite]" />
+        <h1 className="text-2xl font-semibold">{title}</h1>
+        <p className="mt-2 text-gray-600">{description}</p>
+      </div>
+    </div>
+  )
+}
