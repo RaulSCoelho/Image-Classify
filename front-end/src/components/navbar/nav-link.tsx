16105fe31a8d0cbd689549e922d43d58ea683d49
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-14 12:44:02 -0300

    nav link close nav menu

--- front-end/src/components/navbar/nav-link.tsx
+++ front-end/src/components/navbar/nav-link.tsx
@@ -1,14 +1,21 @@
-import { Link, LinkProps, NavbarItem, NavbarMenuItem } from '@nextui-org/react'
+import { Link, LinkProps, NavbarItem, NavbarMenuItem, useNavbarContext } from '@nextui-org/react'
+import { PressEvent } from '@react-types/shared'
 import { usePathname } from 'next/navigation'
 
-export function NavLink({ children, href, color, isMenu, ...props }: LinkProps & { isMenu?: boolean }) {
+export function NavLink({ children, href, color, isMenu, onPress, ...props }: LinkProps & { isMenu?: boolean }) {
+  const { setIsMenuOpen } = useNavbarContext()
   const pathname = usePathname()
   const Item = isMenu ? NavbarMenuItem : NavbarItem
   const isActive = pathname === href
 
+  function handlePress(e: PressEvent) {
+    onPress?.(e)
+    setIsMenuOpen(false)
+  }
+
   return (
     <Item isActive={isActive}>
-      <Link href={href} color={color || isActive ? 'primary' : 'foreground'} {...props}>
+      <Link href={href} color={color || isActive ? 'primary' : 'foreground'} onPress={handlePress} {...props}>
         {children}
       </Link>
     </Item>
