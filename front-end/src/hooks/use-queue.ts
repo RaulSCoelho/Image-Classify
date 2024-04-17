7bb65689887e347b27d271c308b8ea7f77fc269d
RaulSCoelho <raul.s.coelho@gmail.com>
2024-04-17 12:17:22 -0300

    useQueue

--- front-end/src/hooks/use-queue.ts
+++ front-end/src/hooks/use-queue.ts
@@ -5,18 +5,20 @@ import { MaybePromise } from '@/types/promise'
 
 import { useArrayState } from './use-array-state'
 
-export function useQueue<T>(action: MaybePromise<(item: T) => void>) {
+export function useQueue<T>(action: MaybePromise<(item: T) => void>, disable: boolean) {
   const queueState = useArrayState<T>([])
   const queue = queueState[0]
   const remove = queueState[1].remove
 
   useEffect(() => {
-    for (let i = 0; i < queue.length; i++) {
-      const item = queue[i]
-      maybePromise(action, item).finally(() => remove(i))
+    if (!disable) {
+      for (let i = 0; i < queue.length; i++) {
+        const item = queue[i]
+        maybePromise(action, item).finally(() => remove(i))
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [queue])
+  }, [queue, disable])
 
   return queueState
 }
